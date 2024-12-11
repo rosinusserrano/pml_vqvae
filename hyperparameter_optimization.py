c137447371c35b6fbb6e2895330ba7593952236e
konni <konstantin.ausborn@protonmail.com>
2024-12-11 13:57:49 +0100

    First try at distributed optimization.

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -1,143 +1,116 @@
-from ax import optimize
 from ax.service.ax_client import AxClient, ObjectiveProperties
-from ax.utils.notebook.plotting import render
 from ax.service.utils.report_utils import exp_to_df
-from submitit import AutoExecutor, LocalJob, DebugJob
 
 import submitit
-from typing_extensions import NamedTuple
 from time import sleep
-from os import listdir
-
-# import logging
-# submitit.logger.setLevel(logging.DEBUG)
 
 
-class SlurmJob(NamedTuple):
-    """Represent a job scheduled"""
-
-    id: int
-    parameters: dict
-
-
-def test(params):
-    import socket
-
-    print(socket.gethostname())
-    listdir("/")
+def test(parameters):
     sleep(10)
-    import socket
-
-    print(socket.gethostname())
-
-    return params["a"] * params["b"]
+    return parameters["learning_rate"] / parameters["beta_discrete_code_commitment"]
 
 
 class SlurmJobQueueClient:
-    def schedule_job(self, parameters):
+    def __init__(self):
         log_folder = "log_run/%j"
         running_dir = "/home/pml11/github_pml/"
-        executor = submitit.AutoExecutor(
+        self.training_executor = submitit.AutoExecutor(
             folder=log_folder,
             cluster="slurm",
-            slurm_python="/usr/bin/apptainer run --nv --env-file .env --bind /home/space/datasets:/home/space/datasets pml.sif python",
+            slurm_python="/usr/bin/apptainer run --nv --env-file .env --bind "
+            "/home/space/datasets:/home/space/datasets pml.sif python",
         )
-        executor.update_parameters(
+        self.training_executor.update_parameters(
             slurm_partition="cpu-2h",
             # slurm_gpus_per_node=1,
             slurm_cpus_per_task=4,
             slurm_job_name="hyper_param_opt",
             slurm_additional_parameters={
                 "chdir": running_dir,
-                "export": "ALL,PATH=$PATH:/usr/bin/python3:/opt/slurm/bin",
             },
         )
-        print("We are here")
-        job = executor.submit(test, parameters)
-        print(job.job_id)
-        print(job._tasks)
-        print(job.result())
-        print(job.paths)
-        output = 5
-        print(output)
 
-
-"""• beta in loss parameter
-• PixelCNN parameters (tdb)
-2.2 Training Hyperparameters
-• epochs/early stoppage
-• batch size"""
+    def submit_training_job(self, parameters):
+        try:
+            job = self.training_executor.submit(test, parameters)
+        except:
+            return False
+        return job
 
 
 def evaluate(parameters):
     return parameters["beta_discrete_code_commitment"] * parameters["learning_rate"]
 
 
-ax_client = AxClient()
-ax_client.create_experiment(
-    name="hyper_param_optimization",
-    parameters=[
-        {
-            "name": "hidden_dimensions",
-            "type": "choice",
-            "values": [16, 32, 64, 128, 256, 512],
-            "sort_values": True,
-            "is_ordered": True,
-        },
-        {
-            "name": "codebook_size",
-            "type": "choice",
-            "values": [64, 128, 256, 512, 1024, 2048],
-            "sort_values": True,
-            "is_ordered": True,
-        },
-        {
-            "name": "beta_discrete_code_commitment",
-            "type": "range",
-            "bounds": [0.01, 2.0],
-        },
-        {
-            "name": "optimizer",
-            "type": "choice",
-            "values": ["adam", "stochastic_gd", "adagrad"],
-            "is_ordered": False,
-            "sort_values": False,
-        },
-        {
-            "name": "learning_rate",
-            "type": "range",
-            "bounds": [1e-6, 0.1],
-            "log_scale": True,
-        },
-        {
-            "name": "batch_size",
-            "type": "choice",
-            "values": [32, 64, 128, 256, 512, 1024, 2048],
-            "sort_values": True,
-            "is_ordered": True,
-        },
-    ],
-    objectives={"mse": ObjectiveProperties(minimize=True)},
-)
-print(ax_client.get_max_parallelism())
-
-for i in range(50):
-    parametrization, trial_index = ax_client.get_next_trial()
-    if trial_index is not None:
-        ax_client.complete_trial(
-            trial_index=trial_index, raw_data=evaluate(parametrization)
-        )
-best, values = ax_client.get_best_parameters()
-render(
-    ax_client.get_contour_plot(
-        param_x="learning_rate", param_y="beta_discrete_code_commitment"
+def main():
+    ax_client = AxClient()
+    ax_client.create_experiment(
+        name="hyper_param_optimization",
+        parameters=[
+            {
+                "name": "hidden_dimensions",
+                "type": "choice",
+                "values": [16, 32, 64, 128, 256, 512],
+                "sort_values": True,
+                "is_ordered": True,
+            },
+            {
+                "name": "codebook_size",
+                "type": "choice",
+                "values": [64, 128, 256, 512, 1024, 2048],
+                "sort_values": True,
+                "is_ordered": True,
+            },
+            {
+                "name": "beta_discrete_code_commitment",
+                "type": "range",
+                "bounds": [0.01, 2.0],
+            },
+            {
+                "name": "optimizer",
+                "type": "choice",
+                "values": ["adam", "stochastic_gd", "adagrad"],
+                "is_ordered": False,
+                "sort_values": False,
+            },
+            {
+                "name": "learning_rate",
+                "type": "range",
+                "bounds": [1e-6, 0.1],
+                "log_scale": True,
+            },
+            {
+                "name": "batch_size",
+                "type": "choice",
+                "values": [32, 64, 128, 256, 512, 1024, 2048],
+                "sort_values": True,
+                "is_ordered": True,
+            },
+        ],
+        objectives={"mse": ObjectiveProperties(minimize=True)},
     )
-)
-print(best, values)
-"""
-import socket
-print(socket.gethostname())
 
-client = SlurmJobQueueClient()
-client.schedule_job({"a": 5, "b": 6})
-"""
+    slurm_queue_client = SlurmJobQueueClient()
+
+    total_budget = 20
+    num_parallel_jobs = 4
+    jobs = []
+    submitted_jobs = 0
+
+    while submitted_jobs < total_budget or jobs:
+        for job, trial_index in jobs[:]:
+            if job.done():
+                result = job.result()
+                ax_client.complete_trial(trial_index=trial_index, raw_data=result)
+                jobs.remove((job, trial_index))
+        if submitted_jobs < total_budget and len(jobs) < num_parallel_jobs:
+            parameters_next, trial_index_next = ax_client.get_next_trial()
+
+            job = slurm_queue_client.submit_training_job(parameters_next)
+            print(f"Submitted as {job.job_id}")
+            submitted_jobs += 1
+            jobs.append((job, trial_index_next))
+
+        print(exp_to_df(ax_client.experiment))
+
+        sleep(5)
