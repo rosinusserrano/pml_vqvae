5b9f84de3bddf17b1922f75ffa31ab862c1cc2a7
konni <konstantin.ausborn@protonmail.com>
2025-01-15 14:53:00 +0100

    quick change

--- src/pml_vqvae/evaluation.py
+++ src/pml_vqvae/evaluation.py
@@ -122,7 +122,7 @@ def main():
         "imagenet", n_train=1000, n_test=1000, seed=42, batch_size=64
     )
     print("Loaded.")
-    image_tensor = test_loader[0]
+    image_tensor = next(iter(test_loader))
 
     encoder_outputs = []
     codebooks = []
